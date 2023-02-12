b26a7860c96f125746dcb59884aee4f1f956cd39
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-02-12 17:20:49 +0300

    locators

---
+++ src/test/java/Locators2.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class Locators2 {
+    public static void main(String[] args) throws InterruptedException {
+        String name = "bumerangRahul";
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\burak\\Desktop\\Burak\\Selenium_RahulShetty\\RahulCourse\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys(name);
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("signInBtn")).click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("p")).getText());
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"You are successfully logged in.");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),"Hello "+name+",");
+    }
+}
