2bcc801b6a1afd3812b604e72ae49348a21aeb6e
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-02-15 17:38:10 +0300

    Dropdowns

---
+++ src/test/java/Navigate/UpdatedDropdown.java
@@ -0,0 +1,26 @@
+package Navigate;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropdown {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\burak\\Desktop\\Burak\\Selenium_RahulShetty\\RahulCourse\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(3000);
+
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText()); // 1 Adult
+        int i = 1;
+        while (i<5)
+        {
+            driver.findElement(By.id("hrefIncAdt")).click();
+            i++;
+        }
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText()); // 5 Adult
+    }
+}
