fa1fd703e7339e0f2e025dcd6cb1a09e6db4fc82
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-02-15 15:12:14 +0300

    locators and siblings

--- src/test/java/Locators/LocatorSiblings.java
+++ src/test/java/Locators/LocatorSiblings.java
@@ -1,8 +1,10 @@
+package Locators;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LocatorSiblings {
+public class    LocatorSiblings {
             public static void main(String[] args) {
                 System.setProperty("webdriver.chrome.driver","C:\\Users\\burak\\Desktop\\Burak\\Selenium_RahulShetty\\RahulCourse\\Drivers\\chromedriver.exe");
                 WebDriver driver = new ChromeDriver();
