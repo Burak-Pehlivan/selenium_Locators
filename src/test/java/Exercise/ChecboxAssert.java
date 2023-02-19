44c02afa7c07a1586201147229f2dc80b19ec6fb
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-02-19 19:21:21 +0300

    exercise

--- src/test/java/Exercise/ChecboxAssert.java
+++ src/test/java/Exercise/ChecboxAssert.java
@@ -1,15 +1,49 @@
 package Exercise;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class ChecboxAssert {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\burak\\Desktop\\Burak\\Selenium_RahulShetty\\RahulCourse\\Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
+        /** Checkbox list  */
+        // List<WebElement> checboxTicks = driver.findElements(By.cssSelector("input[id*='checkBoxOption']"));
+
+        /**  First checkbox select verify  */
+        driver.findElement(By.id("checkBoxOption1")).click();
+        System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());
+        Assert.assertEquals(driver.findElement(By.id("checkBoxOption1")).isSelected(),true);
+
+        /**  Wait 3 sec */
+        Thread.sleep(3000);
+
+        /** First checkbox unselect verify */
+        driver.findElement(By.id("checkBoxOption1")).click();
+        System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());
+        Assert.assertEquals(driver.findElement(By.id("checkBoxOption1")).isSelected(),false);
+
+        /**  Wait 3 sec */
+        Thread.sleep(3000);
+
+        /** Checbox count */
+
+        System.out.println(driver.findElements(By.cssSelector("input[id*='checkBoxOption']")).size());
+
+        /** Quit driver , so we can understand that our project is succesfully done*/
+        driver.quit();
+        }
+
+
 
     }
-}
+
---
+++ src/test/java/Exercise/Q2_UI_Dropdowns_EditBoxes_ErrorValidation_Assignment.java
@@ -0,0 +1,42 @@
+package Exercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Q2_UI_Dropdowns_EditBoxes_ErrorValidation_Assignment {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\burak\\Desktop\\Burak\\Selenium_RahulShetty\\RahulCourse\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+
+        driver.findElement(By.xpath("//div/input[@name='name']")).sendKeys("name");
+        driver.findElement(By.xpath("//div/input[@name='email']")).sendKeys("asdfg@gitmail.com");
+        driver.findElement(By.id("exampleInputPassword1")).sendKeys("1234321");
+        driver.findElement(By.xpath("//div/label[@for='exampleCheck1']")).click();
+
+        /**  Gender dropdown  */
+        System.out.println(driver.findElements(By.cssSelector("div option")).size());
+        driver.findElement(By.cssSelector("div option:nth-child(2)")).click();
+
+
+
+        /** Employment checkbox */
+        driver.findElement(By.id("inlineRadio1")).click();
+        System.out.println("Student selected : " + driver.findElement(By.id("inlineRadio1")).isSelected());
+
+        /** Date Of Birth */
+        driver.findElement(By.xpath("//div[@class='form-group']/input[@name='bday']")).sendKeys("17.12.1994");
+
+        /** submit and assert message */
+
+        driver.findElement(By.cssSelector("input[class*='btn-success']")).click();
+        String submitMessage = "Success! The Form has been submitted successfully!.";
+        System.out.println(driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']")).getText().substring(1,submitMessage.length()+2));
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']")).getText().substring(2),submitMessage);
+    }
+
+}
