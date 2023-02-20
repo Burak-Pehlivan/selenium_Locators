a2cd5dfa0d3b7b60b79b8426fde1a7066bf3dadd
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-02-21 01:43:11 +0300

    Alerts Click

---
+++ src/test/java/Alerts/AlertsClick.java
@@ -0,0 +1,30 @@
+package Alerts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertsClick {
+    public static void main(String[] args) throws InterruptedException {
+
+        String text = "AlertsTest";
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\burak\\Desktop\\Burak\\Selenium_RahulShetty\\RahulCourse\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+
+        driver.findElement(By.id("name")).sendKeys(text);
+        driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        Thread.sleep(3000);
+        driver.switchTo().alert().dismiss();
+
+
+    }
+}
