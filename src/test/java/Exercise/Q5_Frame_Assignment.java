471059476c36d849fc4c5e3f94bb0d2f24ad1802
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-02-25 00:27:51 +0300

    Frame Assignment Exercise

--- src/test/java/Exercise/Q5_Frame_Assignment.java
+++ src/test/java/Exercise/Q5_Frame_Assignment.java
@@ -1,7 +1,9 @@
 package Exercise;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Q5_Frame_Assignment {
     public static void main(String[] args) {
@@ -9,7 +11,15 @@ public class Q5_Frame_Assignment {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Nested Frames")).click();
 
+        driver.switchTo().frame(driver.findElement(By.cssSelector("frame[src='/frame_top']")));
+//        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame(driver.findElement(By.cssSelector("frame[src='/frame_middle']")));
+//        driver.switchTo().frame("frame-middle");
+
+        System.out.println(driver.findElement(By.cssSelector("div[id='content']")).getText());
 
     }
 }
