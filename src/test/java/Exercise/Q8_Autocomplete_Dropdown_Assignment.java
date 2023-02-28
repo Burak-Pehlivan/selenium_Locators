871e0cdc5ba20a24d13c49761f63b17e21fa57cb
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-03-01 00:13:26 +0300

    Autocomplete dropdown, select with down and get Value

---
+++ src/test/java/Exercise/Q8_Autocomplete_Dropdown_Assignment.java
@@ -0,0 +1,29 @@
+package Exercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Q8_Autocomplete_Dropdown_Assignment {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\burak\\Desktop\\Burak\\Selenium_RahulShetty\\RahulCourse\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        driver.get("https://qaclickacademy.com/practice.php");
+
+        driver.findElement(By.cssSelector("input[placeholder='Type to Select Countries']")).sendKeys("ind");
+        Thread.sleep(3000);
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+        System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value"));
+
+    }
+}
